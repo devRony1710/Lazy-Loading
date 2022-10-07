a64cdd5886fa8ccbc011cc09bb3e0c735f5e60ea
Rony Antolinez <rony1710.dev@gmail.com>
2022-10-07 15:47:24 -0400

    build lazy loading [intersection observer]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazyLoading";
 
-console.log('Happy hacking :)')
+const URL = 'https://randomfox.ca/floof/'
+const mountNode = document.querySelector('#images')
+const addImageBtn = document.querySelector('button');
+
+//this function will be create an image node
+const createImageNode = (image) => {
+    const container = document.createElement('div')
+    container.className = 'p-4'
+
+    const imagen = document.createElement('img')
+    imagen.className = 'mx-auto'
+    imagen.width = '320'
+    imagen.src = image
+
+    // added img node to container
+    container.append(imagen)
+    registerImage(imagen)
+    
+    // added container to "mount" node
+    mountNode.append(container)
+}
+
+
+// get data from fox api
+const fetchImage = async urlApi => {
+    const response = await fetch(urlApi)
+    const data = await response.json()
+    let image = data.image
+
+    // send image atribute to createImageNode function like a src to img node
+    createImageNode(image)
+}
+
+fetchImage(URL)
+
+// this function will be create an image when the user clicks at "agregar imagen" button
+const addImage = async () => {
+    const newImage = await fetchImage(URL)
+    return newImage
+}
+
+
+addImageBtn.addEventListener('click', addImage)
